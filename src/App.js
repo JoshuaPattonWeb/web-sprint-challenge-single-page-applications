d1a49c9345c97cf8a7b2ec8a01c036785e831ccb
unknown <uxob03@outlook.com>
2021-11-12 15:43:24 -0500

    created app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+
+//components used
+import Home from '../pages/Home'
+import Form from '../pages/form'
+import Confirmation from '../pages/confirmation'
 
 const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <p>HOT PIZZAS FRESH OUT THE OVEN</p>
     </>
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+      </div>
+      <div>
+        <Link id='order-pizza' to='/pizza'>Form</Link>
+      </div>
+      <Switch>
+        <Route path='/'>
+          <Home />
+        </Route>
+        <Route path='/pizza'>
+        <Form />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 export default App;
